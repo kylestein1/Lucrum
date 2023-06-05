68b05872f926a1f58c5625578223c8a8aea44e72
kylestein1 <kylestein@wustl.edu>
2023-06-05 14:24:53 -0400

    AnnInc

--- lucrum/src/Components/StockInfo.js
+++ lucrum/src/Components/StockInfo.js
@@ -12,6 +12,7 @@ import axios from 'axios';
 const StockInfo = ({ symbol }) => {
     const [stockData, setStockData] = useState(null);
     const [cashFlowData, setCashFlowData] = useState(null);
+    // const [annIncomeData, setAnnIncomeData] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,6 +22,9 @@ const StockInfo = ({ symbol }) => {
 
                 const cashFlowResponse = await axios.get(`https://financialmodelingprep.com/api/v3/cash-flow-statement/${symbol}?apikey=7b2b04af203b96586946293f38387361`);
                 setCashFlowData(cashFlowResponse.data[0]);
+                
+                // const AnnIncomeResponse = await axios.get(`https://financialmodelingprep.com/api/v3/income-statement/${symbol}?apikey=7b2b04af203b96586946293f38387361`);
+                // setAnnIncomeData(AnnIncomeResponse.data[0]);
             } catch (err) {
                 console.error(err);
             }
@@ -53,6 +57,17 @@ const StockInfo = ({ symbol }) => {
                 ))}
                 </tbody>
             </table>
+            {/* <h2>Annual Income Statements Data</h2> */}
+            {/* <table style={styles.table}> */}
+                {/* <tbody> */}
+                {/* {Object.entries(annIncomeData).map(([key, value]) => ( */}
+                     {/* <tr key={key} style={styles.row}> */}
+                        {/* <td style={styles.cell}>{key}</td> */}
+                        {/* <td style={styles.cell}>{value}</td> */}
+                    {/* </tr> */}
+                 {/* ))} */}
+                {/* </tbody> */}
+            {/* </table> */}
         </div>
     );
 }
